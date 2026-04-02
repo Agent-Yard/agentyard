ee37e1bb89a43bfb24f60b3b4247039854d31f5a
Eric <zephor@qq.com>
2026-04-02 17:52:38 +0800

    fix web start

--- scripts/dev-web.sh
+++ scripts/dev-web.sh
@@ -8,4 +8,4 @@ source "$ROOT_DIR/scripts/common-env.sh"
 load_lynxus_env "apps/web"
 
 cd "$ROOT_DIR"
-exec pnpm --filter @lynxus/web dev -- --host "${LYNXUS_WEB_HOST:-0.0.0.0}" --port "${LYNXUS_WEB_PORT:-5173}"
+exec pnpm --filter @lynxus/web dev --host "${LYNXUS_WEB_HOST:-0.0.0.0}" --port "${LYNXUS_WEB_PORT:-5173}"
